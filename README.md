96342e5f4f52a4f4f9a44a40427ac094eec9fa27
william <william_swl@163.com>
2022-12-11 00:22:54 +0800

    feat: add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+[Streamlit Cloud Deployed](https://fortune-judger-qf-web-main-page-z9x093.streamlit.app/)
--- main_page.py
+++ main_page.py
@@ -1,37 +1,3 @@
 import streamlit as st
 import pandas as pd
 st.markdown('# main page')
-
-# index_file = '/home/william/web/QuantFund/02-history_value/index_data/000001_上证指数.csv'
-# DFindex = pd.read_csv(index_file)
-
-slider = st.slider('slider')
-if st.checkbox('show slider results:'):
-    st.write(slider)
-
-sel = st.selectbox('select a number: ', range(5))
-if st.sidebar.checkbox('show select results:'):
-    st.write(sel)
-
-st.radio('Sorting hat', ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
-
-# input
-st.text_input("text input", key="Tx")
-st.write(st.session_state.Tx)
-
-# layout
-col1, col2, col3 = st.sidebar.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
-
-
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
-
-st.dataframe(df)
-st.table(df)
-
-
